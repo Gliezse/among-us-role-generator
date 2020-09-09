05c3aecc8bed1f517d2eb2b90411f4749470e897
Cristian Cruz <ccruz@technisys.com>
2020-09-09 19:19:13 -0300

    Commit de relleno

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+class Home extends Component {
+    render() { 
+        return (
+            <Fragment>
+                <div>
+                    coscu army
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default Home;
